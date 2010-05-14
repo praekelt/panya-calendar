3006b3643c4f319f7098126df82d3ad513e19707
shaunsephton <shaunsephton@gmail.com>
2010-05-14 13:54:46 +0200

    some more save handlers

--- cal/models.py
+++ cal/models.py
@@ -11,7 +11,7 @@ def save_handler_does_not_repeat(entry):
 
     # delete all entry items related to this entry
     # XXX: yes this is not super efficient, but results in the cleanest code.
-    EntryItem.objects.filter(entry=entry).delete()
+    entry.delete_entryitem_set()
 
     # create a single entryitem linked to entry with provided entry's fields
     entry_item = EntryItem(start=entry.start, end=entry.end, entry=entry, content=entry.content)
@@ -19,6 +19,21 @@ def save_handler_does_not_repeat(entry):
 
     for calendar in entry.calendars.all():
         entry_item.calendars.add(calendar)
+
+def day_repeater(entry, allowed_days=[0,1,2,3,4,5,6]):
+    day = entry.start.date()
+    while day <= entry.repeat_until:
+        if day.weekday() in allowed_days:
+            start = entry.start
+            start = start.replace(year=day.year, month=day.month, day=day.day)
+            end = start + entry.duration
+            entry_item = EntryItem(start=start, end=end, entry=entry, content=entry.content)
+            entry_item.save()
+
+            for calendar in entry.calendars.all():
+                entry_item.calendars.add(calendar)
+
+        day = day + timedelta(days=1)
 
 def save_handler_daily(entry):
     # raise an error if wrong handler is triggered
@@ -31,23 +46,105 @@ def save_handler_daily(entry):
 
     # delete all entry items related to this entry
     # XXX: yes this is not super efficient, but results in the cleanest code.
-    EntryItem.objects.filter(entry=entry).delete()
+    entry.delete_entryitem_set()
 
     # create entryitem linked to entry for each day until entry's repeat until value.
+    day_repeater(entry, allowed_days=[0,1,2,3,4,5,6])
+
+def save_handler_weekdays(entry):
+    # raise an error if wrong handler is triggered
+    if entry.repeat != 'weekdays':
+        raise Exception("In handler 'weekdays' for entry with repeat set as '%s'" % entry.repeat)
+    
+    # check for repeat until:
+    if not entry.repeat_until:
+        raise Exception("Entry should provide repeat_until value for 'weekdays' repeat.")
+    
+    # delete all entry items related to this entry
+    # XXX: yes this is not super efficient, but results in the cleanest code.
+    entry.delete_entryitem_set()
+    
+    # create entryitem linked to entry for each weekday until entry's repeat until value.
+    day_repeater(entry, allowed_days=[0,1,2,3,4])
+
+def save_handler_weekends(entry):
+    # raise an error if wrong handler is triggered
+    if entry.repeat != 'weekends':
+        raise Exception("In handler 'weekends' for entry with repeat set as '%s'" % entry.repeat)
+    
+    # check for repeat until:
+    if not entry.repeat_until:
+        raise Exception("Entry should provide repeat_until value for 'weekends' repeat.")
+    
+    # delete all entry items related to this entry
+    # XXX: yes this is not super efficient, but results in the cleanest code.
+    entry.delete_entryitem_set()
+    
+    # create entryitem linked to entry for each weekend day until entry's repeat until value.
+    day_repeater(entry, allowed_days=[5,6])
+
+def save_handler_weekly(entry):
+    # raise an error if wrong handler is triggered
+    if entry.repeat != 'weekly':
+        raise Exception("In handler 'weekly' for entry with repeat set as '%s'" % entry.repeat)
+    
+    # check for repeat until:
+    if not entry.repeat_until:
+        raise Exception("Entry should provide repeat_until value for 'weekly' repeat.")
+    
+    # delete all entry items related to this entry
+    # XXX: yes this is not super efficient, but results in the cleanest code.
+    entry.delete_entryitem_set()
+        
+    # create an entryitem linked to this entry for each week until entry's repeat until value, with the start day being the same for each week.
     day = entry.start.date()
-    duration = entry.end - entry.start
-    while day < entry.repeat_until:
+    while day <= entry.repeat_until:
         start = entry.start
         start = start.replace(year=day.year, month=day.month, day=day.day)
-        end = start + duration
+        end = start + entry.duration
         entry_item = EntryItem(start=start, end=end, entry=entry, content=entry.content)
         entry_item.save()
 
         for calendar in entry.calendars.all():
             entry_item.calendars.add(calendar)
 
-        day = day + timedelta(days=1)
+        day = day + timedelta(days=7)
+
+def save_handler_monthly_by_day_of_month(entry):
+    # raise an error if wrong handler is triggered
+    if entry.repeat != 'monthly_by_day_of_month':
+        raise Exception("In handler 'monthly by day of month' for entry with repeat set as '%s'" % entry.repeat)
     
+    # check for repeat until:
+    if not entry.repeat_until:
+        raise Exception("Entry should provide repeat_until value for 'monthly by day of month' repeat.")
+    
+    # delete all entry items related to this entry
+    # XXX: yes this is not super efficient, but results in the cleanest code.
+    entry.delete_entryitem_set()
+       
+    # create an entryitem linked to entry for each month until entry's repeat until value, with the start day being the same day date of the month for each month.
+    day = entry.start.date()
+    while day <= entry.repeat_until:
+        start = entry.start
+        start = start.replace(year=day.year, month=day.month, day=day.day)
+        end = start + entry.duration
+        entry_item = EntryItem(start=start, end=end, entry=entry, content=entry.content)
+        entry_item.save()
+
+        for calendar in entry.calendars.all():
+            entry_item.calendars.add(calendar)
+
+        # get next valid date
+        valid_date = False
+        i = 1
+        while not valid_date:
+            try: 
+                day = day.replace(year=day.year + (day.month+i)/12, month=(day.month+i)%12, day=day.day)
+                valid_date = True
+            except ValueError:
+                i += 1
+
 class Calendar(ModelBase):
     class Meta():
         verbose_name = "Calendar"
@@ -68,11 +165,18 @@ class Entry(EntryAbstract):
         choices=(
             ('does_not_repeat', 'Does Not Repeat'),
             ('daily', 'Daily'), 
+            ('weekdays', 'Weekdays'),
+            ('weekends', 'Weekends'),
             ('weekly', 'Weekly'), 
-            ('monthly', 'Monthly'), 
-            ('yearly', 'Yearly'),
+            ('monthly_by_day_of_month', 'Monthly By Day Of Month'), 
         ),
-        default='Does Not Repeat',
+        default='does_not_repeat',
+    )
+    # XXX: repeat every is a placeholder for now
+    repeat_every = models.IntegerField(
+        editable=False,
+        blank=True,
+        null=True,
     )
     repeat_until = models.DateField(
         blank=True,
@@ -89,6 +193,10 @@ class Entry(EntryAbstract):
         repeat_handlers = {
             'does_not_repeat': save_handler_does_not_repeat,
             'daily': save_handler_daily,
+            'weekdays': save_handler_weekdays,
+            'weekends': save_handler_weekends,
+            'weekly': save_handler_weekly,
+            'monthly_by_day_of_month': save_handler_monthly_by_day_of_month, 
         }
         repeat_handlers[self.repeat](self)
 
@@ -99,6 +207,13 @@ class Entry(EntryAbstract):
         verbose_name = "Entry"
         verbose_name_plural = "Entries"
 
+    def delete_entryitem_set(self):
+        self.entryitem_set.all().delete()
+
+    @property
+    def duration(self):
+        return self.end - self.start
+        
 class EntryItem(EntryAbstract):
     entry = models.ForeignKey(
         'cal.Entry',
@@ -107,3 +222,10 @@ class EntryItem(EntryAbstract):
         'cal.Calendar',
         related_name='entryitem_calendar'
     )
+
+    def __unicode__(self):
+        return "Entry Item for %s" % self.content.title
+    
+    @property
+    def duration(self):
+        return self.end - self.start
--- cal/tests.py
+++ cal/tests.py
@@ -13,42 +13,42 @@ class EntrySaveHandlersTestCase(unittest.TestCase):
         calendar.save()
         self.calendar = calendar
 
+    def test_save_handler_does_not_repeat(self):
+        # create an entry
         entry = models.Entry(
             start=datetime.now(), 
             end=datetime.now() + timedelta(days=1),
             repeat="does_not_repeat",
-            content=content,
+            content=self.content,
         )
         entry.save()
-        entry.calendars.add(calendar)
-        self.entry = entry
+        entry.calendars.add(self.calendar)
 
-    def test_save_handler_does_not_repeat(self):
         # raise an exception if handler is used with incorrect entry
-        self.entry.repeat="daily"
-        self.failUnlessRaises(Exception, models.save_handler_does_not_repeat, self.entry)
-        self.entry.repeat="does_not_repeat"
+        entry.repeat="daily"
+        self.failUnlessRaises(Exception, models.save_handler_does_not_repeat, entry)
+        entry.repeat="does_not_repeat"
 
         # should create a single entryitem linked to this entry
-        models.save_handler_does_not_repeat(self.entry)
-        entries = models.EntryItem.objects.filter(entry=self.entry)
-        self.failUnlessEqual(entries.get().entry, self.entry)
+        models.save_handler_does_not_repeat(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.get().entry, entry)
         
         # created entry item should point to entry
-        entry = entries.get()
-        self.failUnlessEqual(entry.entry, self.entry)
+        ent = entries.get()
+        self.failUnlessEqual(ent.entry, entry)
 
         # created entry item should have same field values as entry
-        self.failUnlessEqual(entry.start, self.entry.start)
-        self.failUnlessEqual(entry.end, self.entry.end)
-        self.failUnlessEqual(entry.content, self.entry.content)
-        self.failUnlessEqual(entry.entry, self.entry)
-        self.failUnlessEqual(list(entry.calendars.all()), list(self.entry.calendars.all()))
+        self.failUnlessEqual(ent.start, entry.start)
+        self.failUnlessEqual(ent.end, entry.end)
+        self.failUnlessEqual(ent.content, entry.content)
+        self.failUnlessEqual(ent.entry, entry)
+        self.failUnlessEqual(list(ent.calendars.all()), list(entry.calendars.all()))
 
         # subsequent save should still only have a single entryitem linked to this entry 
-        models.save_handler_does_not_repeat(self.entry)
-        entries = models.EntryItem.objects.filter(entry=self.entry)
-        self.failUnlessEqual(entries.get().entry, self.entry)
+        models.save_handler_does_not_repeat(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.get().entry, entry)
     
     def test_save_handler_daily(self):
         # create an entry
@@ -72,22 +72,19 @@ class EntrySaveHandlersTestCase(unittest.TestCase):
         self.failUnlessRaises(Exception, models.save_handler_daily, entry)
 
         # should create an entryitem linked to this entry for each day until entry's repeat until value.
-        entry.repeat_until = (datetime.now() + timedelta(days=30)).date()
+        entry.repeat_until = (entry.start + timedelta(days=30)).date()
         models.save_handler_daily(entry)
         entries = models.EntryItem.objects.filter(entry=entry)
-        self.failUnlessEqual(entries.count(), 30)
+        self.failUnlessEqual(entries.count(), 31)
 
         # check correct generation
+        starts = set()
         entries = entries.order_by('start')
-        i = 0
-        duration = entry.end - entry.start
         for ent in entries:
-            if i != 0:
-                # all entry start values should be seperated by a day
-                self.failUnlessEqual(ent.start, entries[i - 1].start + timedelta(days=1))
-            i += 1
+            starts.add(ent.start)
+            
             # entry end values should be start adjusted by original duration 
-            self.failUnlessEqual(ent.end - ent.start, duration)
+            self.failUnlessEqual(ent.duration, entry.duration)
         
             # created entry item should point to entry
             self.failUnlessEqual(ent.entry, entry)
@@ -95,8 +92,227 @@ class EntrySaveHandlersTestCase(unittest.TestCase):
             # created entry items should have same field values as entry (except for repeating start and ends)
             self.failUnlessEqual(ent.content, entry.content)
             self.failUnlessEqual(list(ent.calendars.all()), list(entry.calendars.all()))
+
+        # entry items should not have the same starts
+        self.failUnlessEqual(len(starts), 31)
            
         # subsequent save should still only create an entryitem linked to this entry for each day until entry's repeat until value.
         models.save_handler_daily(entry)
         entries = models.EntryItem.objects.filter(entry=entry)
-        self.failUnlessEqual(entries.count(), 30)
+        self.failUnlessEqual(entries.count(), 31)
+    
+    def test_save_handler_weekdays(self):
+        handler = models.save_handler_weekdays
+        # create an entry
+        entry = models.Entry(
+            start=datetime(year=2000, month=1, day=1, hour=1, minute=1), 
+            end=datetime(year=2000, month=1, day=1, hour=1, minute=1) + timedelta(days=1),
+            repeat="weekdays",
+            repeat_until = (datetime.now() + timedelta(days=30)).date(),
+            content=self.content,
+        )
+        entry.save()
+        entry.calendars.add(self.calendar)
+        
+        # raise an exception if handler is used with incorrect entry
+        entry.repeat = 'does_not_repeat'
+        self.failUnlessRaises(Exception, handler, entry)
+        entry.repeat='weekdays'
+        
+        # raise an exception if entry does not provide repeat until
+        entry.repeat_until = None
+        self.failUnlessRaises(Exception, handler, entry)
+        
+        # should create an entryitem linked to this entry for each weekday until entry's repeat until value.
+        entry.repeat_until = (entry.start + timedelta(days=30)).date()
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 21)
+        
+        # check correct generation
+        starts = set()
+        for ent in entries:
+            starts.add(ent.start)
+            
+            # only weekdays
+            self.failIf(ent.start.weekday() >= 5)
+            
+            # entry end values should be start adjusted by original duration 
+            self.failUnlessEqual(ent.duration, entry.duration)
+        
+            # created entry item should point to entry
+            self.failUnlessEqual(ent.entry, entry)
+        
+            # created entry items should have same field values as entry (except for repeating start and ends)
+            self.failUnlessEqual(ent.content, entry.content)
+            self.failUnlessEqual(list(ent.calendars.all()), list(entry.calendars.all()))
+        
+        # entry items should not have the same starts
+        self.failUnlessEqual(len(starts), 21)
+        
+        # subsequent save should still only create an entryitem linked to this entry for each weekday until entry's repeat until value.
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 21)
+    
+    def test_save_handler_weekends(self):
+        handler = models.save_handler_weekends
+        # create an entry
+        entry = models.Entry(
+            start=datetime(year=2000, month=1, day=1, hour=1, minute=1), 
+            end=datetime(year=2000, month=1, day=1, hour=1, minute=1) + timedelta(days=1),
+            repeat="weekends",
+            repeat_until = (datetime.now() + timedelta(days=30)).date(),
+            content=self.content,
+        )
+        entry.save()
+        entry.calendars.add(self.calendar)
+        
+        # raise an exception if handler is used with incorrect entry
+        entry.repeat = 'does_not_repeat'
+        self.failUnlessRaises(Exception, handler, entry)
+        entry.repeat='weekends'
+        
+        # raise an exception if entry does not provide repeat until
+        entry.repeat_until = None
+        self.failUnlessRaises(Exception, handler, entry)
+        
+        # should create an entryitem linked to this entry for each weekend day until entry's repeat until value.
+        entry.repeat_until = (entry.start + timedelta(days=30)).date()
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 10)
+        
+        # check correct generation
+        starts = set()
+        for ent in entries:
+            starts.add(ent.start)
+            
+            # only weekend days
+            self.failIf(ent.start.weekday() < 5)
+            
+            # entry end values should be start adjusted by original duration 
+            self.failUnlessEqual(ent.duration, entry.duration)
+        
+            # created entry item should point to entry
+            self.failUnlessEqual(ent.entry, entry)
+        
+            # created entry items should have same field values as entry (except for repeating start and ends)
+            self.failUnlessEqual(ent.content, entry.content)
+            self.failUnlessEqual(list(ent.calendars.all()), list(entry.calendars.all()))
+        
+        # entry items should not have the same starts
+        self.failUnlessEqual(len(starts), 10)
+        
+        # subsequent save should still only create an entryitem linked to this entry for each weekday until entry's repeat until value.
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 10)
+    
+    def test_save_handler_weekly(self):
+        handler = models.save_handler_weekly
+        # create an entry
+        entry = models.Entry(
+            start=datetime(year=2000, month=1, day=1, hour=1, minute=1), 
+            end=datetime(year=2000, month=1, day=1, hour=1, minute=1) + timedelta(days=1),
+            repeat="weekly",
+            repeat_until = (datetime.now() + timedelta(days=30)).date(),
+            content=self.content,
+        )
+        entry.save()
+        entry.calendars.add(self.calendar)
+        
+        # raise an exception if handler is used with incorrect entry
+        entry.repeat = 'does_not_repeat'
+        self.failUnlessRaises(Exception, handler, entry)
+        entry.repeat='weekly'
+        
+        # raise an exception if entry does not provide repeat until
+        entry.repeat_until = None
+        self.failUnlessRaises(Exception, handler, entry)
+        
+        # should create an entryitem linked to this entry for each week until entry's repeat until value, with the start day being the same for each week.
+        entry.repeat_until = (entry.start + timedelta(days=30)).date()
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 5)
+        
+        # check correct generation
+        starts = set()
+        for ent in entries:
+            starts.add(ent.start)
+           
+            # weekdays should be the same
+            self.failUnlessEqual(ent.start.weekday(), entry.start.weekday())
+       
+            # entry end values should be start adjusted by original duration 
+            self.failUnlessEqual(ent.duration, entry.duration)
+        
+            # created entry item should point to entry
+            self.failUnlessEqual(ent.entry, entry)
+        
+            # created entry items should have same field values as entry (except for repeating start and ends)
+            self.failUnlessEqual(ent.content, entry.content)
+            self.failUnlessEqual(list(ent.calendars.all()), list(entry.calendars.all()))
+        
+        # entry items should not have the same starts
+        self.failUnlessEqual(len(starts), 5)
+        
+        # subsequent save should still only create an entryitem linked to this entry for each weekday until entry's repeat until value.
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 5)
+    
+    def test_save_handler_monthly_by_day_of_month(self):
+        handler = models.save_handler_monthly_by_day_of_month
+        # create an entry
+        entry = models.Entry(
+            start=datetime(year=2000, month=1, day=31, hour=1, minute=1), 
+            end=datetime(year=2000, month=2, day=1, hour=1, minute=1) + timedelta(days=1),
+            repeat="monthly_by_day_of_month",
+            repeat_until = (datetime.now() + timedelta(days=30)).date(),
+            content=self.content,
+        )
+        entry.save()
+        entry.calendars.add(self.calendar)
+        
+        # raise an exception if handler is used with incorrect entry
+        entry.repeat = 'does_not_repeat'
+        self.failUnlessRaises(Exception, handler, entry)
+        entry.repeat='monthly_by_day_of_month'
+        
+        # raise an exception if entry does not provide repeat until
+        entry.repeat_until = None
+        self.failUnlessRaises(Exception, handler, entry)
+        
+        # should create an entryitem linked to this entry for each month until entry's repeat until value, with the start day being the same day date of the month for each month.
+        entry.repeat_until = (entry.start + timedelta(days=366)).date()
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 7)
+        
+        # check correct generation
+        starts = set()
+        for ent in entries:
+            starts.add(ent.start)
+           
+            # day dates should be the same
+            self.failUnlessEqual(ent.start.day, entry.start.day)
+       
+            # entry end values should be start adjusted by original duration 
+            self.failUnlessEqual(ent.duration, entry.duration)
+        
+            # created entry item should point to entry
+            self.failUnlessEqual(ent.entry, entry)
+        
+            # created entry items should have same field values as entry (except for repeating start and ends)
+            self.failUnlessEqual(ent.content, entry.content)
+            self.failUnlessEqual(list(ent.calendars.all()), list(entry.calendars.all()))
+        
+        # entry items should not have the same starts
+        self.failUnlessEqual(len(starts), 7)
+        
+        # subsequent save should still only create an entryitem linked to this entry for each month until entry's repeat until value, with the start day being the same day date of the month for each month.
+        handler(entry)
+        entries = models.EntryItem.objects.filter(entry=entry)
+        self.failUnlessEqual(entries.count(), 7)
