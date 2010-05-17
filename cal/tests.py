c5017747a897942156df867696766090956d87fe
shaunsephton <shaunsephton@gmail.com>
2010-05-17 17:41:45 +0200

    manager tests

--- cal/tests.py
+++ cal/tests.py
@@ -1,8 +1,22 @@
 from datetime import datetime, timedelta
 import unittest
 
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.db import models as django_models
+
+
 from cal import models
+from cal.models import Calendar, Entry, EntryItem
 from content.models import ModelBase
+
+class WantedContent(ModelBase):
+    pass
+django_models.register_models('cal', WantedContent)
+
+class UnwantedContent(ModelBase):
+    pass
+django_models.register_models('cal', UnwantedContent)
 
 class EntrySaveHandlersTestCase(unittest.TestCase):
     def setUp(self):
@@ -316,3 +330,183 @@ class EntrySaveHandlersTestCase(unittest.TestCase):
         handler(entry)
         entries = models.EntryItem.objects.filter(entry=entry)
         self.failUnlessEqual(entries.count(), 7)
+
+class PermittedManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        # create website site item and set as current site
+        self.web_site = Site(domain="web.address.com")
+        self.web_site.save()
+        settings.SITE_ID = self.web_site.id
+
+    def test_get_query_set(self):
+        # create unpublished calendar
+        unpublished_cal = Calendar(title='title', state='unpublished')
+        unpublished_cal.save()
+        unpublished_cal.sites.add(self.web_site)
+        unpublished_cal.save()
+        
+        # create staging calendar
+        staging_cal = Calendar(title='title', state='staging')
+        staging_cal.save()
+        staging_cal.sites.add(self.web_site)
+        staging_cal.save()
+        
+        # create published calendar
+        published_cal = Calendar(title='title', state='published')
+        published_cal.save()
+        published_cal.sites.add(self.web_site)
+        published_cal.save()
+        
+        # create unpublished content
+        unpublished_content = ModelBase(title='title', state='unpublished')
+        unpublished_content.save()
+        unpublished_content.sites.add(self.web_site)
+        unpublished_content.save()
+        
+        # create staging content
+        staging_content = ModelBase(title='title', state='staging')
+        staging_content.save()
+        staging_content.sites.add(self.web_site)
+        staging_content.save()
+        
+        # create published content
+        published_content = ModelBase(title='title', state='published')
+        published_content.save()
+        published_content.sites.add(self.web_site)
+        published_content.save()
+
+        # entries with unpublished calendars and content should not be available in queryset
+        # create unpublished cal and content entries
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=unpublished_content)
+        entry_obj.save()
+        entry_obj.calendars.add(unpublished_cal)
+        entry_obj.save()
+        queryset = EntryItem.permitted.all()
+        self.failIf(queryset.count())
+        Entry.objects.all().delete()
+        
+        # entries with unpublished calendars should not be available in queryset, regardless of content state
+        # create unpublished cal, published content entries
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=published_content)
+        entry_obj.save()
+        entry_obj.calendars.add(unpublished_cal)
+        entry_obj.save()
+        queryset = EntryItem.permitted.all()
+        self.failIf(queryset.count())
+        Entry.objects.all().delete()
+        
+        # entries with unpublished content should not be available in queryset, regardless of cal state
+        # create unpublished content, published cal entries
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=unpublished_content)
+        entry_obj.save()
+        entry_obj.calendars.add(published_cal)
+        entry_obj.save()
+        queryset = EntryItem.permitted.all()
+        self.failIf(queryset.count())
+        Entry.objects.all().delete()
+        
+        # entries with staging calendars and content should be available in queryset but only if settings.STAGING = True
+        # create staging cal and content entries
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=staging_content)
+        entry_obj.save()
+        entry_obj.calendars.add(staging_cal)
+        entry_obj.save()
+        settings.STAGING = False
+        queryset = EntryItem.permitted.all()
+        self.failIf(queryset.count())
+        settings.STAGING = True
+        queryset = EntryItem.permitted.all()
+        self.failUnless(queryset.count())
+        Entry.objects.all().delete()
+        
+        # entries with published cal and content should be available in queryset
+        # create published cal and content entries
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=published_content)
+        entry_obj.save()
+        entry_obj.calendars.add(published_cal)
+        entry_obj.save()
+        queryset = EntryItem.permitted.all()
+        self.failUnless(queryset.count())
+        Entry.objects.all().delete()
+
+        # queryset should not contain items for other sites
+        mobile_site = Site(domain="mobi.address.com")
+        mobile_site.save()
+        # create published calendar for mobile site
+        published_cal_mobile = Calendar(title='title', state='published')
+        published_cal_mobile.save()
+        published_cal_mobile.sites.add(mobile_site)
+        published_cal_mobile.save()
+        # create published calendar for mobile site
+        published_content_mobile = ModelBase(title='title', state='published')
+        published_content_mobile.save()
+        published_content_mobile.sites.add(mobile_site)
+        published_content_mobile.save()
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=published_content_mobile)
+        entry_obj.save()
+        entry_obj.calendars.add(published_cal_mobile)
+        entry_obj.save()
+        queryset = EntryItem.permitted.all()
+        self.failIf(queryset.count())
+
+    def test_by_model(self):
+        # create published calendar
+        published_cal = Calendar(title='title', state='published')
+        published_cal.save()
+        published_cal.sites.add(self.web_site)
+        published_cal.save()
+        
+        # create published wanted content
+        wanted_content = WantedContent(title='title', state='published')
+        wanted_content.save()
+        wanted_content.sites.add(self.web_site)
+        wanted_content.save()
+        
+        # create published unwanted content
+        unwanted_content = UnwantedContent(title='title', state='published')
+        unwanted_content.save()
+        unwanted_content.sites.add(self.web_site)
+        unwanted_content.save()
+        
+        # create entries for wanted
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=wanted_content)
+        entry_obj.save()
+        entry_obj.calendars.add(published_cal)
+        entry_obj.save()
+        
+        # create entries for unwanted
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=unwanted_content)
+        entry_obj.save()
+        entry_obj.calendars.add(published_cal)
+        entry_obj.save()
+
+        # should only return entry items for content of the provided model.
+        queryset = EntryItem.permitted.by_model(WantedContent)
+        for obj in queryset:
+            self.failUnlessEqual(obj.content.class_name, 'WantedContent')
+    
+    def test_now(self):
+        # create published calendar
+        published_cal = Calendar(title='title', state='published')
+        published_cal.save()
+        published_cal.sites.add(self.web_site)
+        published_cal.save()
+        
+        # create published content
+        content = ModelBase(title='title', state='published')
+        content.save()
+        content.sites.add(self.web_site)
+        content.save()
+        
+        # create entries
+        entry_obj = Entry(start=datetime.now(), end=datetime.now() + timedelta(days=1), repeat="daily", repeat_until=(datetime.now() + timedelta(days=30)).date(), content=content)
+        entry_obj.save()
+        entry_obj.calendars.add(published_cal)
+        entry_obj.save()
+
+        # should return currently active entry items, ordered by start
+        queryset = EntryItem.permitted.now()
+        self.failUnless(queryset.count())
+        for entry_item in queryset:
+            self.failUnless(entry_item.start < datetime.now())
+            self.failUnless(entry_item.end > datetime.now())
