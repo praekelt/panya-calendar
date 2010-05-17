4ca3cd8b4058851ca27e0832408bff64a919ce32
shaunsephton <shaunsephton@gmail.com>
2010-05-17 17:42:25 +0200

    list display

--- cal/admin.py
+++ cal/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from cal.models import Calendar, Entry
 from content.admin import ModelBaseAdmin
 
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('content', 'start', 'end', 'repeat', 'repeat_until')
+    list_filter = ('repeat',)
+    search_fields = ('content__title', 'content__description')
+
 admin.site.register(Calendar, ModelBaseAdmin)
-admin.site.register(Entry)
+admin.site.register(Entry, EntryAdmin)
